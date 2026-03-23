df42991dfc2769a403d7d376a7b552556e8f0720
afkllc <22saqlaini@utcsheffield.org.uk>
2026-03-23 18:19:01 +0000

    Updated references

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import swup from "@swup/astro";
-
+import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
